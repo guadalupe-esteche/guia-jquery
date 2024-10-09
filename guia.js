a131c22276041de6dd68d16414f34d99b389c0c3
guadalupe-esteche <estecheguada@gmail.com>
2024-10-09 20:00:59 -0300

    cuarto commit

--- guia.js
+++ guia.js
@@ -55,7 +55,7 @@ function presionElemento (){
 }
 */
 
-/* ejercicio 6 */
+/* ejercicio 6 
 function inicializarEvento () {
     let p = $("#boton1");
     p.click(tamañoPequeño);
@@ -77,4 +77,49 @@ function tamañoGrande(){
     let g = $("descripcion_noticia")
     g.css("font-size", "30px")
 }
+*/
 
+/* ejercicio 7 
+function inicializarEvento () {
+    let b_= $("#boton4");
+    b_.click(cambiarContenido);
+}
+
+function cambiarContenido (){
+    let c= $("#tabla3 td"); 
+    c.text("-");
+}
+*/
+
+/* ejercicio 8 
+function inicializarEvento () {
+    let b_1= $("#boton5");
+    b_1.click(hipervinculoInstagram);
+    let b_2= $("#boton6");
+    b_2.click(hipervinculoFacebook);
+    let b_3= $("#boton7");
+    b_3.click(hipervinculoX);
+}
+
+function hipervinculoInstagram(){
+    let b_1= $("a");
+    b_1.attr("href", "https://www.instagram.com/");
+    b_1.text("Instagram");
+}
+
+function hipervinculoFacebook(){
+    let b_1= $("a");
+    b_1.attr("href", "https://www.facebook.com/");
+    b_1.text("Facebook");
+}
+
+function hipervinculoX(){
+    let b_1= $("a");
+    b_1.attr("href", "https://x.com/?lang=es");
+    b_1.text("X");
+}
+*/
+
+/* ejercicio 9 */
+
+/* ejercicio 10 */
